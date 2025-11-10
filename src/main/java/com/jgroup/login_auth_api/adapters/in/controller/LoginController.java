9d6f9f534fd7d1f8790527fdf859e8282807c9a9
Juan Gabriel <65375925+Juan-Gabriel8@users.noreply.github.com>
2025-11-10 12:18:51 -0300

    Refactoring/architecture refactor (#2)
    refactoring architecture + config Token

--- src/main/java/com/jgroup/login_auth_api/adapters/in/controller/LoginController.java
+++
@@ -1,7 +0,0 @@
-package com.jgroup.login_auth_api.adapters.in.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class LoginController {
-}
--- src/main/java/com/jgroup/login_auth_api/adapters/out/repositories/RoleJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jgroup.login_auth_api.adapters.out.repositories;
-
-import com.jgroup.login_auth_api.app.core.domain.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleJpaRepository extends JpaRepository<Role, Long> {
-
-    Role findByName(String name);
-}
